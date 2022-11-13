2a38b9bf34d557164240c6b7d1a261808e26f168
msohaibb <44452143+msohaibb@users.noreply.github.com>
2022-11-13 14:03:54 -0500

    added DC Motor PID Control

---
+++ DCMotor_PIDControl/angular_position_control.py
@@ -0,0 +1,81 @@
+import numpy as np
+import parameters as P
+from integrators import get_integrator
+from pid import PIDControl
+import matplotlib.pyplot as plt
+
+
+# f(t, x, u) = (Ku - y) / tau
+
+def f(t, y, u):
+
+    # TODO what in God's good name is happening here
+    return np.array([y[1], (1 / P.tau) * (P.K * u - y[0])])
+
+
+class Controller:
+    def __init__(self):
+        kp = P.kp
+        ki = P.ki
+        kd = P.kd
+        limit = P.umax
+        sigma = P.sigma
+        Ts = P.Ts
+        self.controller = PIDControl(kp, ki, kd, limit, sigma, Ts, flag=True)
+
+    def update(self, r, y):
+        return self.controller.PID(r, y)
+
+
+class System:
+    def __init__(self):
+        self.integrator = get_integrator(P.Ts, f, integrator="RK4")
+
+    def update(self, u):
+        return self.integrator.step(t, y, u)
+
+
+
+# Init system and feedback controller
+system = System()
+controller = Controller()
+
+# Simulate step response
+
+
+r = 5
+y = np.array([0, 0])
+t = 0
+
+t_history = [0]
+y_history = [y]
+u_history = [0]
+
+for i in range(P.nsteps):
+
+    u = controller.update(r, y.item(1))
+    y = system.update(u)
+    t += P.Ts
+
+    t_history.append(t)
+    y_history.append(y)
+    u_history.append(u)
+
+y_ = np.asarray(y_history)
+
+# Plot response y due to step change in r
+plt.close('all')
+
+fig, ax = plt.subplots()
+ax.plot(t_history, y_[:,1], label='Motor Position', color='orange')
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Angular Position (rad)')
+plt.axhline(y=r, color='r', linestyle='--', label='Commanded Position')
+plt.legend()
+
+
+ax2 = ax.twinx()
+ax2.plot(t_history, u_history, label='Input Voltage', color='green')
+ax2.set_ylabel('Actuation Signal (V)')
+plt.legend()
+plt.show()
---
+++ DCMotor_PIDControl/cascaded_angular_speed_control.py
@@ -0,0 +1,84 @@
+import numpy as np
+import parameters as P
+from integrators import get_integrator
+from pid import PIDControl
+import matplotlib.pyplot as plt
+
+
+# f(t, x, u) = (Ku - y) / tau
+
+def f(t, y, u):
+    # mass sprint system state space form:
+    return (P.K * u - y) / P.tau
+
+
+class Controller:
+    def __init__(self):
+        kp = P.kp
+        ki = P.ki
+        kd = P.kd
+        # TODO Look at saturation slope
+        limit = P.emax
+        sigma = P.sigma
+        Ts = P.Ts
+        self.controller = PIDControl(kp, ki, kd, limit, sigma, Ts)
+
+    def update(self, r, y):
+        return self.controller.PID(r, y)
+
+
+class System:
+    def __init__(self):
+        self.integrator = get_integrator(P.Ts, f)
+        self.t_history = [0]
+        self.y_history = [0]
+        pass
+
+    def update(self, u):
+        return self.integrator.step(t, y, u)
+
+        pass
+
+
+# Init system and feedback controller
+system = System()
+controller = Controller()
+
+# Simulate step response
+
+r = 10
+t = 0
+y = 0
+u = 0
+
+t_history = [t]
+y_history = [y]
+u_history = [u]
+
+for i in range(P.nsteps):
+    u = controller.update(r, y)
+    y = system.update(u)
+    t += P.Ts
+
+    t_history.append(t)
+    y_history.append(y)
+    u_history.append(u)
+
+# Plot response y due to step change in r
+plt.close('all')
+
+
+fig, ax = plt.subplots()
+ax.plot(t_history, y_history, label='Motor Speed', color='orange')
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Angular Velocity (rad/s)')
+plt.axhline(y=r, color='r', linestyle='--', label='Commanded Speed')
+plt.legend()
+
+
+ax2 = ax.twinx()
+ax2.plot(t_history, u_history, label='Input Voltage', color='green')
+ax2.set_ylabel('Actuation Signal (V)')
+plt.legend()
+plt.show()
+
