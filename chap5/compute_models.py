21ae2135577c03bf52df57ca8284d14fcb3bae83
atamayev <actamayev@gmail.com>
2022-12-09 15:33:54 -0500

    make sure to check av1 again

--- chap5/compute_models.py
+++ chap5/compute_models.py
@@ -102,7 +102,8 @@ def compute_tf_model(mav, trim_state, trim_input):
 
     # Compute transfer function coefficients using new propulsion model
     # CHAPTER 5: SLIDE 19: LEC 7-8 LINEAR MODELS
-
+    
+    # REVIEW AV1
     a_V1 = ((MAV.rho * Va_trim * MAV.S) / (MAV.mass)) * (MAV.C_D_0 + MAV.C_D_alpha * alpha_trim + MAV.C_D_delta_e * delta_trim) - dT_dVa(delta_trim, Va_trim)/(MAV.mass)
     a_V2 = dT_ddelta_t(delta_trim, Va_trim) / (MAV.mass)
     a_V3 = MAV.gravity * np.cos(theta_trim - alpha_trim)
