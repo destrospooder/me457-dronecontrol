e85812b2a3d655053862c8c4e01e7faf04a1978d
msohaibb <44452143+msohaibb@users.noreply.github.com>
2022-12-17 04:46:49 -0500

    implementing LQR
    currently trying to implement state-control for autopilot

--- chap6/autopilot.py
+++ chap6/autopilot.py
@@ -15,8 +15,105 @@ from chap6.pd_control_with_rate import PDControlWithRate
 from message_types.msg_state import MsgState
 from message_types.msg_delta import MsgDelta
 
+import sys
+import numpy as np
+from numpy import array, sin, cos, radians, concatenate, zeros, diag
+from scipy.linalg import solve_continuous_are, inv
+sys.path.append('..')
+import parameters.control_parameters as AP
+import chap5.model_coef as M
 
 class Autopilot:
+    def __init__(self, ts_control):
+        self.Ts = ts_control
+
+        self.integratorCourse = 0
+        self.integratorAltitude = 0
+        self.integratorAirspeed = 0
+        self.errorCourseD1 = 0
+        self.errorAltitudeD1 = 0
+        self.errorAirspeedD1 = 0
+
+        CrLat = array([[0, 0, 0, 0, 1.0]])
+        AAlat = concatenate((
+                    concatenate((M.A_lat, zeros((5, 1))), axis=1),
+                    concatenate((CrLat, zeros((1, 1))), axis=1)),
+            axis=0)
+        BBlat = concatenate((M.B_lat, zeros((1, 2))), axis=0)
+        Qlat = diag([0.001, 0.01, 0.1, 100, 1, 100]) # v, p, r, phi, chi, intChi
+        Rlat = diag([1, 1]) #a, r
+        Plat = solve_continuous_are(AAlat, BBlat, Qlat, Rlat)
+        self.Klat = inv(Rlat) @ BBlat.T @ Plat
+        CrLon = array([[0, 0, 0, 0, 1.0], [1/AP.Va0, 1/AP.Va0, 0, 0, 0]])
+        AAlon = concatenate((
+                    concatenate((M.A_lon, zeros((5, 2))), axis=1),
+                    concatenate((CrLon, zeros((2, 2))), axis=1)),
+                axis=0)
+        BBlon = concatenate((M.B_lon, zeros((2, 2))), axis=0)
+        Qlon = diag([10, 10, 0.001, 0.01, 10, 100, 100])
+        Rlon = diag([1, 1])
+        Plon = solve_continuous_are(AAlon, BBlon, Qlon, Rlon)
+        self.Klon = inv(Rlon) @ BBlon.T @ Plon
+        self.commanded_state = MsgState
+
+    def update(self, cmd, state):
+        # lateral autopilot
+        errorAirspeed = state.Va - cmd.airspeed_command
+        chi_c = wrap(cmd.course_command, state.chi)
+        errorCourse = saturate(state.chi - chi_c, -radians(15), radians(15))
+        self.integratorCourse = self.integratorCourse + (self.Ts/2) * (errorCourse + self.errorCourseD1)
+        self.errorCourseD1 = errorCourse
+        xLat = array([[errorAirspeed * sin(state.beta)],
+                      [state.p],
+                      [state.r],
+                      [state.phi],
+                      [errorCourse],
+                      [self.integratorCourse]])
+        tmp = -self.Klat @ xLat
+        delta_a = saturate(tmp.item(0), -radians(30), radians(30))
+        delta_r = saturate(tmp.item(1), -radians(30), radians(30))
+
+        # long. autopilot
+        altitude_c = saturate(cmd.altitude_command,
+                              state.altitude - 0.2*AP.altitude_zone,
+                              state.altitude + 0.2*AP.altitude_zone)
+
+        errorAltitude = state.altitude - altitude_c
+        self.integratorAltitude = self.integratorAltitude + (self.Ts/2) * (errorAltitude + self.errorAltitudeD1)
+        self.errorAltitudeD1 = errorAltitude
+        self.integratorAirspeed = self.integratorAirspeed + (self.Ts / 2) * (errorAirspeed + self.errorAirspeedD1)
+        self.integratorAirspeed = errorAirspeed
+        xLon = array([[errorAirspeed * cos(state.alpha)],
+                      [errorAirspeed * sin(state.alpha)],
+                      [state.q],
+                      [state.theta],
+                      [errorAltitude],
+                      [self.integratorAltitude],
+                      [self.integratorAirspeed]])
+
+        tmp = -self.Klon @ xLon
+        delta_e = saturate(tmp.item(0), -radians(30), radians(30))
+        delta_t = saturate(tmp.item(1), 0.0, 1.0)
+
+        delta = MsgDelta(elevator=delta_e,
+                         aileron=delta_a,
+                         rudder=delta_r,
+                         throttle=delta_t)
+        self.commanded_state.altitude = cmd.altitude_command
+        self.commanded_state.Va = cmd.airspeed_command
+        return delta, self.commanded_state
+
+
+def saturate(input, low_limit, up_limit):
+    if input <= low_limit:
+        output = low_limit
+    elif input >= up_limit:
+        output = up_limit
+    else:
+        output = input
+    return output
+
+"""class Autopilot:
     def __init__(self, ts_control):
         # instantiate lateral controllers
         self.roll_from_aileron = PDControlWithRate(
@@ -78,12 +175,6 @@ class Autopilot:
         self.commanded_state.theta = theta_c
         self.commanded_state.chi = cmd.course_command
         return delta, self.commanded_state
+"""
 
-    def saturate(self, input, low_limit, up_limit):
-        if input <= low_limit:
-            output = low_limit
-        elif input >= up_limit:
-            output = up_limit
-        else:
-            output = input
-        return output
+
